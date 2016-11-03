238c625ac8e01f03539c9c3e5f4a3893954c95a6
angelacff <caifangfangangela@gmail.com>
2016-11-03 13:46:52 +0800

    Update exit.sh

--- test/exit.sh
+++ test/exit.sh
@@ -1,1 +1,8 @@
 #test exit and commands with exit
+angelacai@angelacai-Lenovo-Y50-70$ echo a ||exit
+a
+angelacai@angelacai-Lenovo-Y50-70$ echo a;exit
+a
+angelacai@angelacai-Lenovo-Y50-70:~/下载/rshell/src$ ./r
+angelacai@angelacai-Lenovo-Y50-70$ exit
+angelacai@angelacai-Lenovo-Y50-70:~/下载/rshell/src$ 
