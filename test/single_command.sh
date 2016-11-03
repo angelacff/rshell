145e04f4d3e630f1b6ae7130cac820c9162e808f
angelacff <caifangfangangela@gmail.com>
2016-11-03 13:42:46 +0800

    Update single_command.sh

--- test/single_command.sh
+++ test/single_command.sh
@@ -1,1 +1,7 @@
 
+angelacai@angelacai-Lenovo-Y50-70$ ls -a
+.  ..  makefile  r  rshell  rshell.cpp
+angelacai@angelacai-Lenovo-Y50-70$ ls
+makefile  r  rshell  rshell.cpp
+angelacai@angelacai-Lenovo-Y50-70$ mkdir test
+angelacai@angelacai-Lenovo-Y50-70$ git status
