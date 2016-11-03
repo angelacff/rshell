aa6a43fc3b1cfb572e64c3e08ad245772efdf909
angelacff <caifangfangangela@gmail.com>
2016-11-03 13:44:32 +0800

    Update multi_command.sh

--- test/multi_command.sh
+++ test/multi_command.sh
@@ -1,1 +1,19 @@
-
+#tests commands with ;, && , or ||
+angelacai@angelacai-Lenovo-Y50-70$ echo a;echo b
+a
+b
+angelacai@angelacai-Lenovo-Y50-70$ echo a&&echo b
+a
+b
+angelacai@angelacai-Lenovo-Y50-70$ echo a || echo b
+a
+angelacai@angelacai-Lenovo-Y50-70$ echo a ; echo b&& echo c
+a
+b
+c
+angelacai@angelacai-Lenovo-Y50-70$ echo a || echo b&& echo c
+a
+angelacai@angelacai-Lenovo-Y50-70$ echo a && echo b ||echo c
+a
+b
+c
